a53c9ed3d13f1fa55d5cce6af8331bb031e1f139
kuisatz <kuisatzhaderah@gmail.com>
2017-12-14 14:59:56 +0300

    OgretmenProgramindakiDersler

--- vendor/Slim/SlimHmacProxy.php
+++ vendor/Slim/SlimHmacProxy.php
@@ -93,7 +93,7 @@ class SlimHmacProxy extends \Proxy\Proxy {
                                     'SinavdaKullanilanKitaplar_mbllogin' => 'restApiDefaultCall', 
                                     'OgrenciSinaviSonuclariKaydet_mbllogin' => 'restApiDefaultCall',
                                     'OgrencininSinavlistesi_mbllogin' => 'restApiDefaultCall',
-                                    'OgretmenDersProgramiCombo_mbllogin' => 'restApiDefaultCall',
+                                    'OgretmenProgramindakiDersler_mbllogin' => 'restApiDefaultCall',
         
         
         
